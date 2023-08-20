c4ef439bbe15471a2fc79494ea37df47b7901af7
Kartik Arora <90651659+KartikAroraOfficial@users.noreply.github.com>
2023-08-20 11:31:46 +0000

    Update dev_env_deb_linux.sh

--- dev_env_deb_linux.sh
+++ dev_env_deb_linux.sh
@@ -7,7 +7,6 @@ fi
 
 # update and upgrade
 sudo apt update && sudo apt full-upgrade -y
-sudo apt install 
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb -y
